28240d808fc9b9ef75889aa5afb189ae36af8174
homeira kalim <homeirakalim@gmail.com>
2025-04-28 12:31:22 -0400

    js sending data with fetch lab complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(userName, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: email,
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Server response:", data);
+      
+        const body = document.querySelector("body"); //get the body
+        const p = document.createElement("p");       //create new <p>
+        p.textContent = `ID: ${data.id}`;            //set its text
+        p.style.fontSize = "24px";         
+        p.style.color = "green";           
+        body.appendChild(p);                         //add it to the page
+      })
+      
+    .catch(function (error) {
+        const body = document.querySelector("body");
+        const p = document.createElement("p");
+        p.textContent = error.message;
+        body.appendChild(p);
+    });
+}
